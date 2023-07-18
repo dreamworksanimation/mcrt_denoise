a27c59f398fdae1856e4de9718efdd1d8a958332
mrday <mike.day@dreamworks.com>
2023-07-18 14:26:58 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '3.5'
+    _version = '3.6'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -68,7 +68,7 @@ requires = [
   'cuda-12.1.0.x',
   'openimagedenoise-2.0.0.x',
   'optix-7.3.0.x',
-  'scene_rdl2-12.5'
+  'scene_rdl2-12.6'
 ]
 
 private_build_requires = [
