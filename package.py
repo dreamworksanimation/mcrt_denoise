1b69825f85361d54544108a0baf300bb637e98e3
rpacker <randy.packer@dreamworks.com>
2023-11-14 15:13:13 -0800

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '4.1'
+    _version = '4.2'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
