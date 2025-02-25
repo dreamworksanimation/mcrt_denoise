39154ae3156503795c72c83fb6631a52cc53c56c
206531218_nbcuni <toshi.kato@dreamworks.com>
2025-02-25 15:04:45 -0800

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '6.2'
+    _version = '6.3'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -57,7 +57,7 @@ requires = [
   'cuda-12.1.0.x',
   'openimagedenoise-2.0.0.x',
   'optix-7.6.0.x',
-  'scene_rdl2-15.2'
+  'scene_rdl2-15.3'
 ]
 
 private_build_requires = [
